6b2b85e846ec2fb2db2161314681cffd4deb635b
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2026-01-11 13:37:14 +0100

    test: add mock renderer in list test

--- cmd/list_test.go
+++ cmd/list_test.go
@@ -8,13 +8,36 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func newListSUT() *cobra.Command {
-	deps := domain.Dependencies{}
-	return NewListCmd(deps)
+type mockConfigStore struct {}
+func (s mockConfigStore) GetManagedPaths() ([]string, error) {
+	return []string{"mock path 1"}, nil
+}
+
+type mockRenderer struct {
+	result string
+}
+func (r *mockRenderer) RenderWorktrees(worktrees []domain.Worktree) {
+	r.result = "test"
+}
+
+type mockGit struct {}
+func (g mockGit) GetWorktreesFromPath(path string) []domain.Worktree {
+	return []domain.Worktree{
+		{Path: "Example path one"},
+	}
+}
+
+func newListSUT(renderer mockRenderer) (*cobra.Command, *mockRenderer) {
+	deps := domain.Dependencies{
+		ConfigStore: mockConfigStore{},
+		Renderer: &renderer,
+		Git: mockGit{},
+	}
+	return NewListCmd(deps), &renderer
 }
 
 func TestListVersion(t *testing.T) {
-	sut := newListSUT()
+	sut, _ := newListSUT(mockRenderer{})
 	const expectedVersion = "0.1.0"
 	if sut.Version != expectedVersion {
 		t.Errorf("Expected version %v, got %v instead.", expectedVersion, sut.Version)
@@ -22,9 +45,12 @@ func TestListVersion(t *testing.T) {
 }
 
 func TestListCommand(t *testing.T) {
-	sut := newListSUT()
+	sut, renderer := newListSUT(mockRenderer{})
 	err := sut.Execute()
 	if err != nil {
 		t.Errorf("Expected to pass with no error, got %v insted", err)
+	}
+	if renderer.result != "test" {
+		t.Errorf("Expected 'result', received %v instead", renderer.result)
 	}
 }
