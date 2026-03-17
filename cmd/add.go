ab6c285c09b2c50a4088a2a15ef2904b02840217
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2026-03-17 23:19:02 +0100

    refactor: use filepath to calculate absolute directory

--- cmd/add.go
+++ cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"path/filepath"
 	"wt/internal/debuglog"
 	"wt/internal/domain"
 
@@ -15,14 +16,16 @@ func NewAddCmd(deps domain.Dependencies) *cobra.Command {
 		Short:   "Add new repo to managed",
 		Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			path := args[0]
+			path, err := filepath.Abs(args[0])
+			if err != nil {
+				return fmt.Errorf("resolving path: %w", err)
+			}
 			debuglog.Printf("Adding path %v to managed paths", path)
 			deps.Git.IsGitRepo(path)
 			if !deps.Git.IsGitRepo(path) {
 				return fmt.Errorf("provided path is not a git repository")
 			}
-			err := deps.ConfigStore.AddManagedPath(path)
-			return err
+			return deps.ConfigStore.AddManagedPath(path)
 		},
 	}
 	return cmd
